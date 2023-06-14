894e2191954cea053a9640983f84a7448f5013f9
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-06-14 21:17:36 +0100

    Add explanatory comments

--- src/projects.py
+++ src/projects.py
@@ -1,10 +1,7 @@
 import time
-import json
 from pprint import PrettyPrinter
 
 from utils import get_driver, wait_until_reached, await_element
-
-from selenium.webdriver.common.by import By
 
 pp = PrettyPrinter(indent=4)
 
@@ -28,20 +25,40 @@ def main():
     driver.refresh()
     time.sleep(1)
 
+    # This is the list that will contain all the "projects", i.e. all the
+    # dictionaries containg information on each project's.
+    aggregate_data = []
+
+    # The data returned in this call is akin to the data you see in the
+    # unclicked staff proposer tables. It returns a list of dictionaries
+    # such as the below
+    """
+    {
+        "title": "Connectivity of interval temporal networks",
+        "theme": 162,
+        "staff": "jxfn92",
+        "initials": "EA",
+        "forename": "Eleni",
+        "surname": "Akrida"
+    }
+    """
     all_projects = driver.execute_script(
         'return await $.ajax ({type: "GET",dataType: "json", url: "Registers.php",data: { "query": "allProjects"}})'
     )
 
-    aggregate_data = []
-
     for project in all_projects:
+        # This is a parameter that is passed to the $.ajax call.
         id = project["theme"]
+
+        # For some reason, even though we're specifying we want the data for
+        # one project, it returns a dictionary within a list. Hence the "[0]".
         in_depth_info = driver.execute_script(
             'return await $.ajax ({type: "GET",dataType: "json", url: "Registers.php",data: { "query": "oneProject", "theme":'
             + str(id)
             + "}})"
         )[0]
 
+        # This essentially merges the two dictionaries into one.
         new_dict = project | in_depth_info
 
         aggregate_data.append(new_dict)
