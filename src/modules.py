98112599223feef04abcb0960dca60d97ed5e261
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-06-23 19:02:56 +0100

    Add function to scrape module names and specific page urls

--- src/modules.py
+++ src/modules.py
@@ -1,18 +1,67 @@
-MODULES_SITE_URL = "https://durhamuniversity.sharepoint.com/teams/ComputerScienceUndergraduateCommunity/SitePages/Level-3-M.aspx"
-
 import time
 from pprint import PrettyPrinter
+
+from selenium.webdriver.common.by import By
 
 from utils import (
     get_driver,
     wait_until_reached,
     await_element,
     parse_dotenv,
+    find_el_by_inner_text,
+    get_el_parent,
 )
 
 pp = PrettyPrinter(indent=4)
 
 MODULES_SITE_URL = "https://durhamuniversity.sharepoint.com/teams/ComputerScienceUndergraduateCommunity/SitePages/Level-3-M.aspx"
+
+
+def get_module_page_urls(driver):
+    assert driver.current_url == MODULES_SITE_URL
+
+    # There is a bullet-point list near the top of the page of all the
+    # modules offered. We want to get that list. It doesn't have an ID,
+    # so the best way is probably to find the <p> element above it, which
+    # contains a chunk of text. We can search for the phrase "Below are
+    # links to outlines for all level 3 Computer Science modules available
+    # in 2023-24", then get the parent of that element, then find the <ul>
+    # within that parent.
+
+    p_above_ul = find_el_by_inner_text(
+        driver,
+        "Below are links to outlines for all level 3 Computer Science modules available in 2023-24",
+    )
+
+    p_parent = get_el_parent(p_above_ul)
+    lis = p_parent.find_elements(By.TAG_NAME, "li")
+
+    module_names_and_urls = {}
+
+    for li in lis:
+        # Each <li> has a descendant <a>. This <a> has an attribute 'href'
+        # which obviously tells you on which page the module information
+        # can be found.
+
+        a = li.find_element(By.XPATH, ".//a")
+
+        # This is the name of the module. It'll look something like this:
+        # "COMP3567 Multimedia and Game Development" (without the quotation
+        # marks obviously).
+        name = a.get_attribute("innerText")
+
+        # Some module names include the number of credits, e.g. "COMP3591
+        # Project Preparation (20 Credits)". Obviously we don't want the
+        # part mentioning the credits. Doing [:-13] gets rid of the part in
+        # brackets and the whitespace in front of the opening bracket.
+        if "0 Credits)" in name:
+            name = name[:-13]
+
+        href = a.get_attribute("href")
+
+        module_names_and_urls[name] = href
+
+    return module_names_and_urls
 
 
 def scrape_raw_data():
@@ -40,7 +89,7 @@ def scrape_raw_data():
     wait_until_reached(driver, MODULES_SITE_URL)
     time.sleep(1)
 
-    input()
+    pp.pprint(get_module_page_urls(driver))
 
 
 def main():
