084a1975c57001a44ea73849fd6d09bf3d682602
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-06-26 22:53:38 +0100

    Get module timetable params with user-facing keys

--- src/timetable.py
+++ src/timetable.py
@@ -7,6 +7,8 @@ pp = PrettyPrinter(indent=4)
 from utils import (
     get_driver,
     login_to_page_with_url_auth,
+    get_parent,
+    get_previous_sibling,
 )
 
 MODULE_TIMETABLE_URL = "https://timetable.dur.ac.uk/module.htm"
@@ -30,6 +32,13 @@ def main():
         # but ideally we want to get the label for this <select>.
         name = select.get_attribute("name")
 
+        # This <select> is located within a <table>. More specifically, it's
+        # a <td> within a <tr>. And the previous <td> sibling to this <td>
+        # is the <td> whose inner text is the label that we want.
+        parent_td = get_parent(select)
+        sibling_td = get_previous_sibling(parent_td)
+        label = sibling_td.text
+
         subfield_params = {}
 
         options = select.find_elements(By.TAG_NAME, "option")
@@ -38,13 +47,17 @@ def main():
             # The first is its "value" attribute which stores the value that
             # the server will recognise.
             # The second is the innerText value, which is what the user sees.
+            # e.g: <option value="44">w/c Mon 13 May 2024 - week 44</option>
 
             key = option.text
             value = option.get_attribute("value")
 
             subfield_params[key] = value
-        param_fields[name] = subfield_params
 
+        # param_fields[name] = subfield_params
+        param_fields[label] = subfield_params
+
+    pp.pprint(param_fields)
     return param_fields
 
 
