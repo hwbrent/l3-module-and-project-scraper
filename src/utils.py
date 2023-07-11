e207feb661d035586f68dd08d6620e1fdffa0ca4
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-07-11 23:03:32 +0100

    Copy code over from main branch

--- src/utils.py
+++ src/utils.py
@@ -18,17 +18,15 @@ _project_root = os.path.abspath(os.path.join(_this_directory, os.pardir))
 CHROMEDRIVER_PATH = os.path.join(_project_root, "chromedriver")
 
 
-def get_driver(options: list[str] = []) -> Chrome:
+def get_driver() -> Chrome:
     """Initialises a new instance of the Chrome webdriver and returns it."""
     # Create options object.
     # Allows you to configure options (who would have thought)
     # e.g. whether chrome should run headlessly (is that a word?).
-    # options.add_argument("--headless")
-    driver_opts = Options()
-    for option in options:
-        driver_opts.add_argument(option)
+    options = Options()
+    options.add_argument("--headless")
 
-    driver = Chrome(executable_path=CHROMEDRIVER_PATH, options=driver_opts)
+    driver = Chrome(executable_path=CHROMEDRIVER_PATH, options=options)
 
     return driver
 
