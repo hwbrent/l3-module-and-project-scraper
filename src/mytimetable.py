6e6bd7f62e6a0a955a0f2dfd464366e0501200a5
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-09-13 18:03:18 +0100

    Copy week pattern scraper logic from `timetable.py` branch

--- src/mytimetable.py
+++ src/mytimetable.py
@@ -2,6 +2,56 @@ import utils
 
 URL = "https://mytimetable.durham.ac.uk/weekly/activities"
 
+WEEK_PATTERNS_URL = "https://timetable.dur.ac.uk/week_patterns.htm"
+
+
+def scrape_raw_week_patterns(driver) -> list[dict]:
+    """
+    Scrapes the week pattern data and returns it in a `list` of `dict`s,
+    where each `dict` represents a week (or a row in the `<table>` found
+    on the week patterns webpage).
+    """
+    url = utils.get_url_with_auth(WEEK_PATTERNS_URL)
+    driver.get(url)
+
+    # This is the structure of the table:
+    # +–––––––––––––––––––––––––––––+––––––––––––––––––––––+
+    # |       Syllabus Weeks        |   Durham Weeks       |
+    # +–––––––––––––+–––––––––––––––+––––––+–––––––––––––––+
+    # | Week Number | Calendar Date | Term | Teaching Week |
+    # +–––––––––––––+–––––––––––––––+––––––+–––––––––––––––+
+    # |    Week 1   |               |      |               |
+    # +-------------+---------------+------+---------------+
+    # etcetera
+
+    # "Syllabus Weeks", "Durham Weeks", "Week Number", "Calendar Date"
+    # "Term", and "Teaching Week" are all in their own <th> elements.
+    all_headers = []
+    for th in driver.find_elements(By.TAG_NAME, "th"):
+        header = th.text.strip()
+        all_headers.append(header)
+
+    # This is just the "Week Number", "Calendar Date", "Term", and "Teaching Week"
+    col_headers = all_headers[2:]
+
+    data = []
+
+    # • The date info is stored in <tr>s in the overall <table>.
+    # • The first two <tr>s are in the table header, so we ignore those.
+    trs = driver.find_elements(By.TAG_NAME, "tr")
+    for tr in trs[2:]:
+        row_data = {}
+
+        # Each <tr> has one <td> for each column
+        tds = tr.find_elements(By.TAG_NAME, "td")
+
+        for header, td in zip(col_headers, tds):
+            cell_value = td.text.strip()
+            row_data[header] = cell_value
+        data.append(row_data)
+
+    return data
+
 
 def main():
     driver = utils.get_driver()
