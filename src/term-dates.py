c08d26070019cd0c7d04847d062108248f14823e
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-09-18 19:32:49 +0100

    Begin writing function to scrape term dates data

--- src/term-dates.py
+++ src/term-dates.py
@@ -1,8 +1,19 @@
+import requests
+from pprint import PrettyPrinter
+
+pp = PrettyPrinter(indent=4)
+
 URL = "https://www.durham.ac.uk/academic-dates/"
 
 
-def main():
+def scrape_term_dates():
+    response = requests.get(URL)
+    pp.pprint(response.text)
     pass
+
+
+def main():
+    term_dates = scrape_term_dates()
 
 
 if __name__ == "__main__":
